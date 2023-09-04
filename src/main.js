24df0dfd67e546bc46afee857fb830d1b53cb4b1
tomneozed <tomneozed@gmail.com>
2023-09-04 14:01:42 +0200

    Added List of DCM idols

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 class Main {
+    #people = [
+        'Robert Smith',
+        'Billy Idol',
+        'Nina Hagen',
+        'Sid Vicious'
+    ]
+
     constructor() {
         this.#run()
     }
@@ -6,6 +13,22 @@ class Main {
     #run() {
         const title = document.querySelector('h1')
         title.innerText = 'CD Application'
+
+        const title2 = document.createElement('h2')
+        title2.innerText = 'Au revoir'
+        document.body.appendChild(title2)
+
+        const ul = document.createElement('ul')
+        for (const p of this.#people) {
+            const li = document.createElement('li')
+            if (p !== 'Billy Idol') {
+                li.innerText = p
+            } else {
+                li.innerHTML = `<strong>${p}</strong>`
+            }
+            ul.appendChild(li)    
+        }
+        document.body.appendChild(ul)
     }
 }
 
