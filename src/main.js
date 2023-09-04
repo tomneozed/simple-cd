46da5dd455f8b3c0e4f77a56993902886b1bdc62
tomneozed <tomneozed@gmail.com>
2023-09-04 15:03:21 +0200

    Added Lists + Tables

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 class Main {
     #people = [
-        'Robert Smith',
-        'Billy Idol',
-        'Nina Hagen',
-        'Sid Vicious'
+        {
+            "lastname": "Smith",
+            "firstname": "Robert",
+            "band": "The Cure"
+        },
+        {
+            "lastname": "Idol",
+            "firstname": "Billy",
+            "band": "Generation X"            
+        },
+        {
+            "lastname": "Hagen",
+            "firstname": "Nina",
+            "band": "Nina Hagen"            
+        },
+        {
+            "lastname": "Vicious",
+            "firstname": "Sid",
+            "band": "Sex Pistols"            
+        }
     ]
 
     constructor() {
@@ -11,6 +27,8 @@ class Main {
     }
 
     #run() {
+
+        // Les Titres
         const title = document.querySelector('h1')
         title.innerText = 'CD Application'
 
@@ -18,17 +36,37 @@ class Main {
         title2.innerText = 'Au revoir'
         document.body.appendChild(title2)
 
+        // Les listes
         const ul = document.createElement('ul')
-        for (const p of this.#people) {
+        this.#people.forEach((p) => {
             const li = document.createElement('li')
             if (p !== 'Billy Idol') {
                 li.innerText = p
             } else {
                 li.innerHTML = `<strong>${p}</strong>`
             }
-            ul.appendChild(li)    
-        }
+            ul.appendChild(li)
+        }) 
         document.body.appendChild(ul)
+
+        // Les Tables 
+        const table = document.querySelector('tbody')
+        this.#people.forEach((p) => {
+            const tr = document.createElement('tr')
+            const tdLastname = document.createElement('td')
+            const tdFirstname = document.createElement('td')
+            const tdBand = document.createElement('td')
+            tdLastname.innerText = p.lastname
+            tdFirstname.innerText = p.firstname
+            tdBand.innerText = p.band
+            tr.appendChild(tdLastname)
+            tr.appendChild(tdFirstname)
+            tr.appendChild(tdBand)
+            table.appendChild(tr)
+        })
+
+
+
     }
 }
 
